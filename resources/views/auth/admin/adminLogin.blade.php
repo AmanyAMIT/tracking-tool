aff83b449fa462f1a65b692a59ef880e96e3fc22
AmanyAMIT <amany.amit2@gmail.com>
2022-02-23 19:33:17 +0200

    Tracking Tool

--- resources/views/auth/admin/adminLogin.blade.php
+++ resources/views/auth/admin/adminLogin.blade.php
@@ -1,73 +1,89 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Basic Page Info -->
+	<meta charset="utf-8">
+	<title>Admin Login</title>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Admin Login</div>
+	<!-- Site favicon -->
+	<link rel="apple-touch-icon" sizes="180x180" href="{{URL::asset('AdminPanel/vendors/images/apple-touch-icon.png')}}">
+	<link rel="icon" type="image/png" sizes="32x32" href="{{URL::asset('AdminPanel/vendors/images/favicon-32x32.png')}}">
+	<link rel="icon" type="image/png" sizes="16x16" href="{{URL::asset('AdminPanel/vendors/images/favicon-16x16.png')}}">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('admin-access') }}">
-                        @csrf
+	<!-- Mobile Specific Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+	<!-- Google Font -->
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="{{URL::asset('AdminPanel/vendors/styles/core.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{URL::asset('AdminPanel/vendors/styles/icon-font.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{URL::asset('AdminPanel/vendors/styles/style.css')}}">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119386393-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
 
+		gtag('config', 'UA-119386393-1');
+	</script>
+</head>
+<body class="login-page">
+
+	<div class="login-wrap d-flex align-items-center flex-wrap justify-content-center">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-md-6 col-lg-7">
+					<img src="{{URL::asset('AdminPanel/vendors/images/login-page-img.png')}}" alt="">
+				</div>
+				<div class="col-md-6 col-lg-5">
+					<div class="login-box bg-white box-shadow border-radius-10">
+						<div class="login-title">
+							<h2 class="text-center text-primary">Login To Tracking Tool</h2>
+						</div>
+						<form method="POST" action="{{ route('admin-access') }}">
+                            @csrf
+							<div class="input-group custom">
+								<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="John@doe.com">
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
+								<div class="input-group-append custom">
+									<span class="input-group-text"><i class="icon-copy dw dw-user1"></i></span>
+								</div>
+							</div>
+							<div class="input-group custom">
+								<input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="**********">
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+								<div class="input-group-append custom">
+									<span class="input-group-text"><i class="dw dw-padlock1"></i></span>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-sm-12">
+									<div class="input-group mb-0">
+										<button type="submit" class="btn btn-primary btn-lg btn-block">Sign In</button>
+									</div>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- js -->
+	<script src="{{URL::asset('AdminPanel/vendors/scripts/core.js')}}"></script>
+	<script src="{{URL::asset('AdminPanel/vendors/scripts/script.min.js')}}"></script>
+	<script src="{{URL::asset('AdminPanel/vendors/scripts/process.js')}}"></script>
+	<script src="{{URL::asset('AdminPanel/vendors/scripts/layout-settings.js')}}"></script>
+</body>
+</html>
